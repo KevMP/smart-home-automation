dd1d9baefb89e72483ee1cb0f378606f897d8e97
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-06-28 12:22:05 -0400

    removed the checkroom function.

--- environment.py
+++ environment.py
@@ -1,5 +1,5 @@
 def detectPerson(room):
-    return True
+    return 1
 
 def getTemperature():
     temperature = 0
@@ -8,11 +8,6 @@ def getTemperature():
 def getHumidity():
     humidity = 0
     return humidity
-
-def checkRooms(rooms):
-    for room in rooms:
-        count = detectPerson(room)
-    return [count, "each room a person was detected in"]
 
 def readData(file):
     return ["array of data"]
