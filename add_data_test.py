209603fa9980b6dc90a3c75ec711918f52942f37
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-10 01:43:20 -0400

    function not necessary.

--- add_data_test.py
+++ add_data_test.py
@@ -6,9 +6,6 @@ TABLE_SYSTEM_DATA = 'acSystemData'
 TABLE_SENSOR_DATA = 'sensorData'
 TABLE_USER_DATA = 'userData'
 
-def add_sensor_data():
-    pass
-
 connection = sqlite3.connect(DATABASE)
 cursor = connection.cursor()
 
