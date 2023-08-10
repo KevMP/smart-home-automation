2b3d0f8ccf96fbb9d4e8efaa2b3e9047733308b2
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-10 02:12:18 -0400

    established the functionality to drop the existing table.

--- reset_table.py
+++ reset_table.py
@@ -7,7 +7,8 @@ TABLE_SENSOR_DATA = 'sensorData'
 TABLE_USER_DATA = 'userData'
 
 def drop_existing_table(table):
-    pass
+    drop_table_query = f"DROP TABLE IF EXISTS {table}"
+    cursor.execute(drop_table_query)
 
 def create_sensor_data_table():
     create_table_query = '''
