582f08b93b938a8e7512af7e8237ae1a0262553e
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-10 01:50:21 -0400

    established some variables for the new data being inserted into the database.
    commited the data to the database, will have to clear out later.
    
    established an query that the cursor could call.

--- add_data_test.py
+++ add_data_test.py
@@ -9,5 +9,14 @@ TABLE_USER_DATA = 'userData'
 connection = sqlite3.connect(DATABASE)
 cursor = connection.cursor()
 
+new_temperature = 94.0
+user_id = '1'
+timestamp = '14'
+
+insert_query = f"INSERT INTO {TABLE_SENSOR_DATA} (temperature, userId, timestamp) VALUES (?, ?, ?)"
+
+cursor.execute(insert_query, (new_temperature, user_id, timestamp))
+connection.commit()
+
 cursor.close()
 connection.close()
