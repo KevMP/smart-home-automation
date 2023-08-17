7e16677b12bff0a9b2e088834c5860621d565a93
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-17 18:00:26 -0400

    imported os for file pathing.
    established the constant variables for the file paths.

--- app/establish_table.py
+++ app/establish_table.py
@@ -1,6 +1,12 @@
 import sqlite3
+import os
 
 class Table():
+    APPLICATION_FOLDER = 'app'
+    DATABASES_FOLDER = os.path.join(APPLICATION_FOLDER, 'databases')
+    BACKUP_SCHEMAS_FOLDER = os.path.join(DATABASES_FOLDER, 'backup_schemas')
+    SENSOR_DATA_TABLE_CREATION_QUERY = os.path.join(BACKUP_SCHEMAS_FOLDER, 'sensor_data_table_creation_query.txt')
+
     def drop_table(table):
         drop_table_query = f"DROP TABLE IF EXISTS {table}"
         return drop_table_query
