a13c63355e8e6db4538e0f4ace6e8396a4e59c28
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-17 17:42:50 -0400

    established the function to create the table, parameters assigned are the table_query which is the scheme of the table, and the name of the table.

--- create_table.py
+++ create_table.py
@@ -4,3 +4,6 @@ class Table():
     def drop_table(table):
         drop_table_query = f"DROP TABLE IF EXISTS {table}"
         cursor.execute(drop_table_query)
+    
+    def create_table(table_query, table_name):
+        pass
