2448a444c3aa2a004868464a4134a42183ff1c40
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-17 17:47:54 -0400

    returns the drop table queries.

--- establish_table.py
+++ establish_table.py
@@ -3,8 +3,8 @@ import sqlite3
 class Table():
     def drop_table(table):
         drop_table_query = f"DROP TABLE IF EXISTS {table}"
-        cursor.execute(drop_table_query)
+        return drop_table_query
     
     def create_table(table_query, table_name):
         table_query.format(table_name)
-        cursor.execute(table_query)
+        return table_query
