835e61fd54e2083134e0166f5d7dc0b5f38f1ef1
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-18 11:05:51 -0400

    removed the table addresses file.

--- app/table_addresses.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Address():
-    APPLICATION_FOLDER = 'app'
-    DATABASES_FOLDER = os.path.join(APPLICATION_FOLDER, 'databases')
-    BACKUP_SCHEMAS_FOLDER = os.path.join(DATABASES_FOLDER, 'backup_schemas')
