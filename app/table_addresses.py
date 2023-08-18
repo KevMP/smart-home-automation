d2e6b702054d1b52af460dcb38c1a21ee313f8d2
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-18 11:04:08 -0400

    imported the addresses from the establish table python file.

--- app/table_addresses.py
+++ app/table_addresses.py
@@ -1,4 +1,6 @@
 import os
 
 class Address():
-    pass
+    APPLICATION_FOLDER = 'app'
+    DATABASES_FOLDER = os.path.join(APPLICATION_FOLDER, 'databases')
+    BACKUP_SCHEMAS_FOLDER = os.path.join(DATABASES_FOLDER, 'backup_schemas')
