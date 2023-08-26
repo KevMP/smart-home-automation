672767fcb15a8d707fa7c7bc529eb83dccd657b2
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-26 14:38:31 -0400

    testing.

--- back-end/database/read_table.py
+++ back-end/database/read_table.py
@@ -44,7 +44,7 @@ class Read():
         return self.temperature
     
     ## Make sure to pass the correct user whose preferred temperature
-    ## the Ai will try to aim for!!!!!!!!!
+    ## the Ai will try to aim for!!!!!!!!
 
     ## Ideally this should be called before the simulation loop runs,
     ## but if you want to change users midway, you can also create
