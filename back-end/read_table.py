9db43706d960097929de3e32d1b17f5848516546
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-26 13:00:02 -0400

    gets the current time using the time library.

--- back-end/read_table.py
+++ back-end/read_table.py
@@ -6,6 +6,9 @@ class Read():
     def read_all_data(self):
         pass
 
+    def get_current_time(self):
+        pass
+
     def get_timestamp(self, cursor_object, table_name):
         self.timestamp_query = f'''SELECT timeStamp FROM {table_name}
                                    WHERE timeStamp = '''
