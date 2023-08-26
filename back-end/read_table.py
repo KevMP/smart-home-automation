3a0b8bc135576b9597a67347e212a7280b93403b
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-26 13:28:26 -0400

    returned the table name parameter.

--- back-end/read_table.py
+++ back-end/read_table.py
@@ -10,8 +10,8 @@ class Read():
         self.current_time = time.strftime("%m:%d:%Y:%H:%M:%S")
         return self.current_time
 
-    def get_timestamp(self, cursor_object, current_time):
-        self.timestamp_query = f'''SELECT timeStamp FROM acSystemData
+    def get_timestamp(self, cursor_object, table_name, current_time):
+        self.timestamp_query = f'''SELECT timeStamp FROM {table_name}
                                    WHERE timeStamp = {current_time}'''
         cursor_object.execute(self.timestamp_query)
         return cursor_object[0]
