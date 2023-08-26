d31105f4de18b043ac9787116dd48b17c39661f9
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-26 12:59:42 -0400

    established a where statement.

--- back-end/read_table.py
+++ back-end/read_table.py
@@ -7,6 +7,7 @@ class Read():
         pass
 
     def get_timestamp(self, cursor_object, table_name):
-        self.timestamp_query = f'''SELECT timeStamp FROM {table_name}'''
+        self.timestamp_query = f'''SELECT timeStamp FROM {table_name}
+                                   WHERE timeStamp = '''
         cursor_object.execute(self.timestamp_query)
         return cursor_object[0]
