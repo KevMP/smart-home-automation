d5bec7d3fded343311387afa84f3685f5525ca02
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-29 19:11:48 -0400

    inserts the email to the table.

--- back-end/database/database.py
+++ back-end/database/database.py
@@ -129,7 +129,7 @@ class SMAH():
         database_conection = SMAH.get_connection()
         sql_cursor = database_conection.cursor()
 
-        sql_cursor.execute(SMAH.create_user_account_table_insert_query('email', ))
+        sql_cursor.execute(SMAH.create_user_account_table_insert_query('email', email))
 
         sql_cursor.close()
         database_conection.close()
