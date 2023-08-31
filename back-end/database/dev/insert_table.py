e60c12fb7cf2c794f3c8ff29cfc0f83a65fd4443
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-08-30 20:30:03 -0400

    replaced file data.

--- back-end/database/dev/insert_table.py
+++ back-end/database/dev/insert_table.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+from database import SMAH
 from flask import g
 
 class Insert():
@@ -17,17 +18,17 @@ class Insert():
     ## Note, that when we are generating fake data we can use these functions
     ## to create that faked data.
 
-    def insert_ac_system_data(self, running_time, time_stamp, user_identification):
+    def create_insert_ac_system_data_query(self, running_time, time_stamp, user_identification):
         self.sql_query = f'''INSERT INTO {self.system_table} (runningTime, timeStamp, userId)
                              VALUES ({running_time}, {time_stamp}, {user_identification});'''
         return self.sql_query
 
-    def insert_sensor_data(self, temperature, user_identification, time_stamp):
+    def create_insert_sensor_data_query(self, temperature, user_identification, time_stamp):
         self.sql_query = f'''INSERT INTO {self.sensor_table} (temperature, userId, timestamp)
                              VALUES ({temperature}, {user_identification}, {time_stamp});'''
         return self.sql_query
     
-    def insert_user_data(self, preferred_temperature, number_of_residents):
+    def create_insert_user_data_query(self, preferred_temperature, number_of_residents):
         self.sql_query = f'''INSERT INTO {self.user_table} (preferredTemperature, numberOfResidents)
                              VALUES ({preferred_temperature}, {number_of_residents});'''
         return self.sql_query
@@ -39,6 +40,42 @@ class Insert():
     def create_connection(self, database_path):
         self.database_connection = sqlite3.connect(database_path)
         return self.database_connection
+    
+    ## The following function will insert the data generated from the queries
+    ## above as separate function calls.
+
+    def insert_ac_system_data(self, running_time, time_stamp, user_identification):
+        self.database_connection = self.create_connection(self.database_file)
+        self.sql_cursor = self.database_connection.cursor()
+
+        self.insert_query = self.create_insert_ac_system_data_query(running_time, time_stamp, user_identification)
+        self.sql_cursor.execute(self.insert_query)
+
+        self.database_connection.commit()
+        self.sql_cursor.close()
+        self.database_connection.close()
+
+    def insert_sensor_data(self, temperature, user_identification, time_stamp):
+        self.database_connection = self.create_connection(self.database_file)
+        self.sql_cursor = self.database_connection.cursor()
+
+        self.insert_query = self.create_insert_sensor_data_query(temperature, user_identification, time_stamp)
+        self.sql_cursor.execute(self.insert_query)
+        
+        self.database_connection.commit()
+        self.sql_cursor.close()
+        self.database_connection.close()
+
+    def insert_user_data(self, preferred_temperature, number_of_residents):
+        self.database_connection = self.create_connection(self.database_file)
+        self.sql_cursor = self.database_connection.cursor()
+
+        self.insert_query = self.create_insert_user_data_query(preferred_temperature, number_of_residents)
+        self.sql_cursor.execute(self.insert_query)
+        
+        self.database_connection.commit()
+        self.sql_cursor.close()
+        self.database_connection.close()
 
     def insert_all_data(self):
         self.database_connection = self.create_connection(self.database_file)
