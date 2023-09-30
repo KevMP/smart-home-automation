528c1a2586e35e8cd8c3b603c29b2db6fda988c4
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-09-30 17:27:53 -0400

    constructed a function to get all the table data.

--- back-end/database/database.py
+++ back-end/database/database.py
@@ -32,6 +32,8 @@ class SMAH():
         return database_connection
 
     ## The following functions are returning all the data in a table, at once.
+    ## The get_all_table_data function inserts all the table data into a hash
+    ## map so that it can be jsonified.
     @staticmethod
     def get_all_system_data(database_connection, cursor_object):
         return Query().return_all(database_connection, cursor_object, 'acSystemData')
@@ -45,5 +47,14 @@ class SMAH():
     def get_all_user_account_data(database_connection, cursor_object):
         return Query().return_all(database_connection, cursor_object, 'userAccount')
     @staticmethod
-    def get_all_table_data():
-        pass
+    def get_all_table_data(db, cursor):
+        table_hash_map = {}
+        table_list = [SMAH().get_all_system_data(db, cursor),
+                     SMAH().get_all_sensor_data(db, cursor),
+                     SMAH().get_all_user_data(db, cursor),
+                     SMAH().get_all_user_account_data(db, cursor)]
+        
+        for table in range(len(table_list)):
+            table_hash_map[table] = table_list[table]
+
+        return table_hash_map
