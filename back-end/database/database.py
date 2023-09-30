1f1c4337be58d0bc4d02d94000efa5ad72a5db44
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-09-30 16:39:43 -0400

    established two functions to get/close a connection to the database.

--- back-end/database/database.py
+++ back-end/database/database.py
@@ -3,4 +3,14 @@ import os
 from flask import g
 
 class SMAH():
-    pass
+    @staticmethod
+    def get_connection():
+        if 'db' not in g:
+            g.db = sqlite3.connect('databases/SHAS.db')
+        return g.db
+
+    @staticmethod
+    def close_connection():
+        db = g.pop('db', None)
+        if db is not None:
+            db.close()
