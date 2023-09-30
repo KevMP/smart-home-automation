49a4b56bada15fe4fafdf1ce95f7da6ef82912e2
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-09-30 16:42:08 -0400

    renamed variable.

--- back-end/app.py
+++ back-end/app.py
@@ -32,9 +32,8 @@ def view_data():
     Endpoint to fetch all the data from the database.
     """
     database_connection = SMAH().get_connection()
-    cursor_connection = database_connection.cursor()
+    cursor_object = database_connection.cursor()
 
-    data = SMAH().select_all_system_data(database_connection, cursor_connection)
     SMAH().close_connection()
     return data
 
