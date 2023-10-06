2e653d77ffced9d2ea3357833a856fea17383533
workordeath <alfonso.oramas@icloud.com>
2023-10-06 10:39:46 -0400

    The window size could be made dynamic later.

--- microcontroller_tkinter/view.py
+++ microcontroller_tkinter/view.py
@@ -6,5 +6,12 @@
 ## Functions:
 ## Manually being able to
 ## control the temperature.
-from tkinter import *
+import tkinter as tk
 from database import DATABASE
+
+def main():
+    applicationView = tk.Tk()
+    applicationView.geometry("720*720")
+    applicationView.update()
+
+    applicationView.mainloop()
