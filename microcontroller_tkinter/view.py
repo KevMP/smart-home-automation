f597a86c6c5bd7472e2010fabbcfd636508edd35
workordeath <alfonso.oramas@icloud.com>
2023-10-06 10:41:53 -0400

    established some 'fake' functions

--- microcontroller_tkinter/view.py
+++ microcontroller_tkinter/view.py
@@ -7,11 +7,16 @@
 ## Manually being able to
 ## control the temperature.
 import tkinter as tk
+import os
 from database import DATABASE
 
 def main():
     applicationView = tk.Tk()
     applicationView.geometry("720*720")
     applicationView.update()
+
+    current_time = DATABASE().getCurrentTime()
+    temperature = DATABASE().getTemperature(current_time)
+    airconditioner_status = DATABASE().getAirconditionerStatus()
 
     applicationView.mainloop()
