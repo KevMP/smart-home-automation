deb74fc09c3485c1fc5ee808ea8c584675cc9324
workordeath <alfonso.oramas@icloud.com>
2023-10-06 14:58:34 -0400

    need to get the amount of features that are going to be fed into the ai.

--- back-end/simulation.py
+++ back-end/simulation.py
@@ -1,1 +1,15 @@
 ## This is going to be the environment for the model.
+
+class Simulation():
+    
+    def getReward():
+        pass
+
+    def getTemperature():
+        pass
+
+    def getHumiditiy():
+        pass
+
+    def setTemperature():
+        pass
