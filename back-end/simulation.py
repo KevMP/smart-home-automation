3c43df9ade693891830b1627c75a0ef8d8247485
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-06 21:04:06 -0400

    established a faux main loop of how the simulation will work.

--- back-end/simulation.py
+++ back-end/simulation.py
@@ -9,8 +9,16 @@ class Simulation():
             
     def environment(self, command):
         self.ac_status = command
-        self.temperatuer_array = [0, 20, 40, 70, 75, 80, 81, 100]
+        self.reward = 0
+        self.temperature_array = [0, 20, 40, 70, 75, 80, 81, 100]
+
+        self.current_temperature = self.temperature_array[random.randint(0, 8)]
+        self.target_temperature = self.temperature_array[random.randint(0, 8)]
+
         for self.generation in range(100):
-            pass
+            if self.current_temperature == self.target_temperature:
+                self.target_temperature = self.temperature_array[random.randint(0, 8)]
+            else:
+                pass
 
         return self.reward
