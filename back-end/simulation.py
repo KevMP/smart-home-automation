a3a4352748bdd2f38508bec4ea8118c87b3a5e3c
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-06 21:17:22 -0400

    renamed variables for clarification.

--- back-end/simulation.py
+++ back-end/simulation.py
@@ -9,7 +9,7 @@ class Simulation():
             return -1
             
     def environment(self, agent):
-        self.reward = 0
+        self.agent_reward = 0
         self.temperature_array = [0, 20, 40, 70, 75, 80, 81, 100]
 
         self.current_temperature = self.temperature_array[random.randint(0, 8)]
@@ -17,14 +17,11 @@ class Simulation():
 
         for self.generation in range(100):
             self.command = self.getCommand(agent)
-            ## if the ai/agent reaches the target temperature,
-            ## we choose a new target temperature, and reward our
-            ## agent.
             if self.current_temperature == self.target_temperature:
                 self.target_temperature = self.temperature_array[random.randint(0, 8)]
-                self.reward += 8
+                self.agent_reward += 8
             else:
                 self.current_temperature += self.chooseDirection(self.command)
-                self.reward -= 1
+                self.agent_reward -= 1
         
-        return self.reward
+        return self.agent_reward
