0fcc00169d1476d90872b6f0edc760e8d2643173
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-06 21:13:41 -0400

    established the main loop.

--- back-end/simulation.py
+++ back-end/simulation.py
@@ -8,7 +8,6 @@ class Simulation():
             return -1
             
     def environment(self, agent):
-        self.ac_status = command
         self.reward = 0
         self.temperature_array = [0, 20, 40, 70, 75, 80, 81, 100]
 
@@ -16,9 +15,25 @@ class Simulation():
         self.target_temperature = self.temperature_array[random.randint(0, 8)]
 
         for self.generation in range(100):
+            self.command = self.getCommand(agent)
+            ## if the ai/agent reaches the target temperature,
+            ## we choose a new target temperature, and reward our
+            ## agent.
             if self.current_temperature == self.target_temperature:
                 self.target_temperature = self.temperature_array[random.randint(0, 8)]
+                self.reward += 8
             else:
-                pass
-
+                ## ai/agent chooses to turn on the ac or
+                ## to turn it off.
+                ##
+                ## (Florida's Climate)
+                ## on decreases the temperature.
+                ## off increases the temperature.
+                if self.command == True:
+                    self.current_temperature += 1
+                    self.reward -= 1
+                elif self.command == False:
+                    self.current_temperature -= 1
+                    self.reward += 1
+        
         return self.reward
