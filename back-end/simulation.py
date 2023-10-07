486b5834ef0af466420554b51c4ea1e3b79a5cff
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-06 20:58:53 -0400

    removed the get ac status function.

--- back-end/simulation.py
+++ back-end/simulation.py
@@ -1,17 +1,11 @@
 import random
 
 class Simulation():
-    def returnCommand(self, command):
-        match command:
-            case 0:
-                Simulation().aircondtionerOn()
-                return True
-            case 1:
-                Simulation().aircondtionerOff()
-                return False
+    def chooseDirection(self, command):
+        pass
             
     def environment(self, command):
-        self.ac_status = self.returnCommand(command)
+        self.ac_status = command
 
         for self.generation in range(100):
             pass
