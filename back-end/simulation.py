32e977dd6f6cf22784eaff985abeee5f5d20f5a3
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-06 20:45:49 -0400

    removed the comment block.

--- back-end/simulation.py
+++ back-end/simulation.py
@@ -11,22 +11,4 @@ class Simulation():
                 return False
             
     def environment(self, command):
-
-        self.reward = 0
-
-        ## if the ac is getting closer to our
-        ## expected temperature
-        self.reward = self.reward + 5
-
-        ## if the ac is not getting closer to our
-        ## expected temperature
-        self.reward = self.reward - 5 ## we could also choose not to punish
-                                      ## the model whatsoever.
-
-        ## what we want to get back is the
-        ## value of our reward, if it is
-        ## greater than the previous value,
-        ## we update the model. If not, we
-        ## don't update it whatsoever.
-
-        return self.reward
+        pass
