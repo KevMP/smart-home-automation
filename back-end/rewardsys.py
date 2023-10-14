5c2e26b0a577dd049763c95036ec46ede6e5ccfb
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-14 19:34:04 -0400

    established the direction of the agent.

--- back-end/rewardsys.py
+++ back-end/rewardsys.py
@@ -1,5 +1,8 @@
-## other simulation to work on the side.
+import random
 
 class RewardSys:
     def __init__(self, agent):
         self.agent = agent
+
+    def setTarget(self, temperatureArray=[0, 2, 4], agentPosition=[2, 3]):
+        self.agentDirection = agentPosition[1] - agentPosition[0]
