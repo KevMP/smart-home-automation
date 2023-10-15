6655a0f8ae927b8aa1cd4b952f4510ef86443482
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-14 21:16:20 -0400

    returned a new target to be passed into our main simulation loop.
    it's based off the direction of our agent, so if the agent is moving in a positive direction (temperature being raised), our new target will be in the opposite direction(a lower temperature target)

--- back-end/rewardsys.py
+++ back-end/rewardsys.py
@@ -11,5 +11,21 @@ class RewardSys:
     def getDirection(self):
         return self.direction
 
-    def setTarget(self, temperatureArray=[0, 2, 4], agentPosition=[2, 3]):
-        self.agentDirection = agentPosition[1] - agentPosition[0]
+    def setTarget(self, temperatureArray=[0, 2, 4]):
+        self.newTarget = temperatureArray[random.randint(0, len(temperatureArray) - 1)]
+        return self.newTarget
+
+    def getNewTargetBasedOnAgentDirection(self, temperatureArray=[0, 2, 4], agentPosition=[2, 3]):
+        self.agentDirection = self.getDirection(agentPosition)
+        self.newTarget = self.setTarget(temperatureArray)
+        match self.agentDirection:
+            case -1:
+                while self.newTarget <= agentPosition[1]:
+                    self.newTarget = self.setTarget(temperatureArray)
+            case 0:
+                while self.newTarget == agentPosition[1]:
+                    self.newTarget = self.setTarget(temperatureArray)
+            case 1:
+                while self.newTarget >= agentPosition[1]:
+                    self.newTarget = self.setTarget(temperatureArray)
+        return self.newTarget
