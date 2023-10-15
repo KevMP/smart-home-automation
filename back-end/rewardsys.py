90055cb4745f30c6d5fc5d00841977a342c9e8e7
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-14 21:17:18 -0400

    formatting.

--- back-end/rewardsys.py
+++ back-end/rewardsys.py
@@ -6,15 +6,13 @@ class RewardSys:
         self.direction = [0,1]
 
     def setDirection(self, agentPosition=[2, 3]):
-        self.direction = agentPosition[1] - agentPosition[0]
-    
+        self.direction = agentPosition[1] - agentPosition[0]    
     def getDirection(self):
         return self.direction
 
     def setTarget(self, temperatureArray=[0, 2, 4]):
         self.newTarget = temperatureArray[random.randint(0, len(temperatureArray) - 1)]
         return self.newTarget
-
     def getNewTargetBasedOnAgentDirection(self, temperatureArray=[0, 2, 4], agentPosition=[2, 3]):
         self.agentDirection = self.getDirection(agentPosition)
         self.newTarget = self.setTarget(temperatureArray)
