b69b5f158b1a527856d12d1a5f0c9696f2e6d92a
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-14 21:32:33 -0400

    returned the direction as a number of -1, 0, or 1.

--- back-end/rewardsys.py
+++ back-end/rewardsys.py
@@ -3,18 +3,18 @@ import random
 class RewardSys:
     def __init__(self, agent=None):
         self.agent = agent
-        self.direction = [0,1]
+        self.direction = [1,0]
 
     def setDirection(self, agentPosition=[2, 3]):
-        self.direction = agentPosition[1] - agentPosition[0]    
+        self.direction = agentPosition[1] - agentPosition[0]
     def getDirection(self):
-        return self.direction
+        return self.direction[1] - self.direction[0]
 
     def setTarget(self, temperatureArray=[0, 2, 4]):
         self.newTarget = temperatureArray[random.randint(0, len(temperatureArray) - 1)]
         return self.newTarget
-    def getTargetBasedOnDirection(self, temperatureArray=[0, 2, 4], agentPosition=[2, 3]):
-        self.agentDirection = self.getDirection(agentPosition)
+    def getTargetBasedOnDirection(self, temperatureArray=[0, 2, 4], agentPosition=[1, 0]):
+        self.agentDirection = self.getDirection()
         self.newTarget = self.setTarget(temperatureArray)
         match self.agentDirection:
             case -1:
