0dd93510aebc578802c5e7efbabc225c777fb9d1
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-10-14 20:43:10 -0400

    created functions to set/get the direction of the agent, from its positions.

--- back-end/rewardsys.py
+++ back-end/rewardsys.py
@@ -3,6 +3,13 @@ import random
 class RewardSys:
     def __init__(self, agent=None):
         self.agent = agent
+        self.direction = [0,1]
+
+    def setDirection(self, agentPosition=[2, 3]):
+        self.direction = agentPosition[1] - agentPosition[0]
+    
+    def getDirection(self):
+        return self.direction
 
     def setTarget(self, temperatureArray=[0, 2, 4], agentPosition=[2, 3]):
         self.agentDirection = agentPosition[1] - agentPosition[0]
