233265e2fdda5f00d93f2bd5e98548da89887ce1
workordeath <alfonso.oramas@icloud.com>
2023-11-09 20:31:09 -0500

    established the fields per table.

---
+++ local/database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class CreateDatabase():
+    def createProfilesTable(self):
+        ## profile ID,
+        ## min preferred temperature,
+        ## max preferred temperature.
+        pass
+
+    def createAiTable(self):
+        ## timestamp,
+        ## profile id,
+        ## current ai move,
+        ## temperature.
+        pass
+
+    def createSensorTable(self):
+        ## sensor ID (1, 2, 3, ... etc),
+        ## temperature,
+        ## humidity,
+        ## timestamp.
+        pass
