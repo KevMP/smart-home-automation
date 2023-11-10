1186d00113e59b36a34a47b2163692e7f368b3e6
Emilio Laurenti <62228030+RedGaming4@users.noreply.github.com>
2023-11-10 00:34:28 -0500

    WIP: Resolve NameError and Path Handling Issues
    Partial fix for the 'gif' id linkage in the KV language string and the implementation of raw strings for Windows file paths. Animation loop integration is underway. More updates to follow.

--- local-gui/main.py
+++ local-gui/main.py
@@ -1,169 +1,158 @@
-from kivy.lang import Builder
-from kivymd.app import MDApp
+from kivy.app import App
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.core.window import Window
-from kivy.animation import Animation
-from kivy.properties import NumericProperty, ListProperty
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.graphics import Color
-from kivy.graphics import Line 
 from kivy.lang import Builder
-from kivymd.app import MDApp
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.properties import NumericProperty, ListProperty
-from kivy.graphics import Color, Line
-from kivy.metrics import dp
+from kivy.utils import get_color_from_hex
+from kivy.clock import Clock
+from kivy.properties import NumericProperty
 
 Window.size = (800, 480)
 
 KV = '''
-BoxLayout:
-    orientation: 'vertical'
+#:import get_color_from_hex kivy.utils.get_color_from_hex
+
+<IconButton>:
+    font_size: '16dp'
+    size_hint: None, None
+    size: '150dp', '150dp'
+    # Placeholder for icon image
     canvas.before:
         Color:
-            rgba: 0, 0, 0, 1
+            rgba: get_color_from_hex('#ffffff')
+        Rectangle:
+            size: self.size
+            pos: self.pos
+            source: 'path/to/icon.png'
+
+<ClearButton@Button>:
+    background_normal: ''
+    background_color: (1, 1, 1, 0)  # RGBA: Fully transparent
+    color: get_color_from_hex('#FFFFFF')
+    size_hint: None, None
+    size: '120dp', '40dp'
+    font_size: '18dp'
+
+BoxLayout:
+    orientation: 'vertical'
+    padding: '20dp'
+    spacing: '20dp'
+    canvas.before:
+        Color:
+            rgba: get_color_from_hex('#000000')
         Rectangle:
             pos: self.pos
             size: self.size
 
-    MDFloatLayout:
-        md_bg_color: 1, 0, 100/255, .1
-    CircularProgressBar:  # Make sure this matches the name in your Python code
-        size_hint: None, None
-        size: dp(200), dp(200)
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-        bar_color: 1, 0, 0  # Change as needed
-        bar_width: dp(10)
-        temperature: 75  # Example temperature
-
-    # Filler widget for spacing
-    Widget:
-        size_hint_y: None
-        height: dp(48)
-
-    # Centered grid of buttons
     GridLayout:
         cols: 4
         size_hint_y: None
         height: self.minimum_height
-        size_hint_x: None
-        width: self.minimum_width
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-        padding: dp(10)
-        spacing: dp(10)
+        spacing: '20dp'
         
-        # First row of buttons
-        MDFillRoundFlatButton:
-            text: "Profile"
-            on_release: app.on_profile()
-        MDFillRoundFlatButton:
-            text: "AI"
-            on_release: app.on_ai()
-        MDFillRoundFlatButton:
-            text: "Alerts"
-            on_release: app.on_alerts_messages()
-        MDFillRoundFlatButton:
-            text: "Service"
-            on_release: app.on_service()
-        
-        # Second row of buttons
-        MDFillRoundFlatButton:
-            text: "Settings"
-            on_release: app.on_settings()
-        MDFillRoundFlatButton:
-            text: "Weather"
-            on_release: app.on_weather()
-        MDFillRoundFlatButton:
-            text: "System"
-            on_release: app.on_system()
-        MDFillRoundFlatButton:
-            text: "Schedules"
-            on_release: app.on_schedules()
+        IconButton:
+            text: 'Custom Profiles'
+            on_release: app.button_pressed('Custom Profiles')
+        IconButton:
+            text: 'Schedules'
+            on_release: app.button_pressed('Schedules')
+        IconButton:
+            text: 'Vacation'
+            on_release: app.button_pressed('Vacation')
+        IconButton:
+            text: 'Settings'
+            on_release: app.button_pressed('Settings')
+        IconButton:
+            text: 'Weather'
+            on_release: app.button_pressed('Weather')
+        IconButton:
+            text: 'Alerts and reminders'
+            on_release: app.button_pressed('Alerts and reminders')
+        IconButton:
+            text: 'System'
+            on_release: app.button_pressed('System')
+        IconButton:
+            text: 'Service'
+            on_release: app.button_pressed('Service')
 
-    # Filler widget for spacing
-    Widget:
+    BoxLayout:
         size_hint_y: None
-        height: dp(48)
+        height: '1dp'
+        canvas.before:
+            Color:
+                rgba: 1, 1, 1, 1  # White color
+            Rectangle:
+                pos: self.pos
+                size: self.size
 
-    # Separator line
-    MDBoxLayout:
+    BoxLayout:
         size_hint_y: None
-        height: "1dp"
-        md_bg_color: app.theme_cls.divider_color
-
-    # Footer with 'Back', 'Info', and 'Done' buttons
-    MDBoxLayout:
-        size_hint_y: None
-        height: dp(69)
-        MDFlatButton:
-            text: "Back"
-            pos_hint: {'center_y': 0.5}
-            on_release: app.on_back()
+        height: '60dp'
+        ClearButton:
+            text: 'Back'
+            on_release: app.footer_button_pressed('Back')
         Widget:
-        MDIconButton:
-            icon: "information-outline"
-            pos_hint: {'center_x': 0.5}
-            on_release: app.on_info()
+            size_hint_x: 1
+        ClearButton:
+            text: 'Home'
+            on_press: root.start_animation()
+            size: '140dp', '50dp'
+            font_size: '22dp'
+            on_release: app.go_to_home()
+     Image: 
+        id: gif
+        source: r'C:\Users\emili\OneDrive\Documents\GitHub\smart-home-automation\local-gui\Assets\House Frames\frame_0.png'
+        center: self.parent.center
+        size_hint: None, None
+        size: 500, 500
+        allow_stretch: False
+
         Widget:
-        MDFlatButton:
-            text: "Done"
-            pos_hint: {'center_y': 0.5}
-            on_release: app.on_done()
+            size_hint_x: 1
+        ClearButton:
+            text: 'Done'
+            on_release: app.footer_button_pressed('Done')
 '''
-class CircularProgressBar(AnchorLayout):
-    bar_color = ListProperty([1, 0, 0])  # Initial color: red
-    bar_width = NumericProperty(10)
-    temperature = NumericProperty(0)  # Default temperature
 
-    def __init__(self, **kwargs):
-        super(CircularProgressBar, self).__init__(**kwargs)
-        self.bind(temperature=self.update_progress)
 
-    def update_progress(self, *args):
-        self.canvas.before.clear()
-        with self.canvas.before:
-            Color(*self.bar_color, 1)  # Set the color for the progress bar
-            Line(circle=(self.center_x, self.center_y, dp(100)), width=self.bar_width, angle_end=(self.temperature * 360) / 100)
+class MyAnimationApp(App):
+    index = NumericProperty(0) #keeping track of current frame
+    print(index)
+    max_index = 27 
 
-def __init__(self, **kwargs):
-    super(CircularProgressBar, self).__init__(**kwargs)
-    self.bind(temperature=self.update_progress)
+    def build(self):
+        self.root = Builder.load_string(KV)
+        return self.root
+    
+    def animate(self, dt):
+        self.index += 1
+        if self.index > self.max_index:
+            self.index = 0 # reset 
+            print("im working!")
+        self.root.ids.gif.source = f'frame_{self.index}.png'
 
-class SmartHomeApp(MDApp):
+    def start_anim(self):
+        Clock.schedule_interval(self.animate, 0.1)
+        print("Im working!")
+
+class IconButton(ButtonBehavior, Label):
+    pass
+
+class SmartThermostatApp(App):
     def build(self):
         return Builder.load_string(KV)
 
-    def on_profile(self):
-        print("Profile button pressed")
+    def button_pressed(self, button_text):
+        print(f'{button_text} button pressed')
 
-    def on_ai(self):
-        print("AI button pressed")
+    def footer_button_pressed(self, button_text):
+        print(f'{button_text} button pressed')
 
-    def on_alerts_messages(self):
-        print("Alerts & Messages button pressed")
-
-    def on_service(self):
-        print("Service button pressed")
-
-    def on_settings(self):
-        print("Settings button pressed")
-
-    def on_weather(self):
-        print("Weather button pressed")
-
-    def on_system(self):
-        print("System button pressed")
-
-    def on_schedules(self):
-        print("Schedules button pressed")
-
-    def on_back(self):
-        print("Back button pressed")
-
-    def on_info(self):
-        print("Info button pressed")
-
-    def on_done(self):
-        print("Done button pressed")
+    def go_to_home(self):
+        print('Home button pressed')
 
 if __name__ == '__main__':
-    SmartHomeApp().run()
+    SmartThermostatApp().run()
