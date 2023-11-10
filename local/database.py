6960f346cf496657a2a3f3401d2ae65affd3a7d1
workordeath <alfonso.oramas@icloud.com>
2023-11-09 21:10:52 -0500

    established the database tables.

--- local/database.py
+++ local/database.py
@@ -1,22 +1,48 @@
 import sqlite3
 
-class CreateDatabase():
-    def createProfilesTable(self):
-        ## profile ID,
-        ## min preferred temperature,
-        ## max preferred temperature.
-        pass
+class Database():
+    def __init__(self, db_name='data.db'):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
 
-    def createAiTable(self):
-        ## timestamp,
-        ## profile id,
-        ## current ai move,
-        ## temperature.
-        pass
+    def createProfilesTable(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS profiles (
+                profile_id INTEGER PRIMARY KEY,
+                min_preferred_temperature REAL,
+                max_preferred_temperature REAL
+            )
+        ''')
+        self.conn.commit()
+
+    def createModelTable(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS model (
+                timestamp TEXT,
+                profile_id INTEGER,
+                current_move TEXT
+            )
+        ''')
+        self.conn.commit()
 
     def createSensorTable(self):
-        ## sensor ID (1, 2, 3, ... etc),
-        ## temperature,
-        ## humidity,
-        ## timestamp.
-        pass
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS sensor (
+                timestamp TEXT,
+                sensor_id INTEGER,
+                temperature REAL,
+                humidity REAL
+            )
+        ''')
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+
+# Example usage
+if __name__ == "__main__":
+    db = Database()
+    db.createProfilesTable()
+    db.createModelTable()
+    db.createSensorTable()
+    db.close()
--- local/simulation.py
+++ local/simulation.py
@@ -6,8 +6,8 @@ class AiModel():
 
     def setProfile(self, profile_identification):
         self.profile = profile_identification
-        self.min = Class().getMinimumPreferredTemperature(self.profile)
-        self.max = Class().getMaximumPreferredTemperature(self.profile)
+        self.min = Database().getMinimumPreferredTemperature(self.profile)
+        self.max = Database().getMaximumPreferredTemperature(self.profile)
 
 def main():
     model = AiModel()
