7f9be2f8180fbac6581cf93fc1a1a2e39d26b3a7
Emilio Laurenti <62228030+RedGaming4@users.noreply.github.com>
2023-11-10 23:56:47 -0500

    Work in Progress: Enhancements to Thermostat UI
    This commit introduces a series of enhancements to the smart thermostat application interface:
    
    - Implemented a grid layout for better organization of feature buttons on the home screen.
    - Integrated MDIcons from the KivyMD library, providing a modern and clean aesthetic to the button design.
    - Increased the size of the icons for improved visibility and user interaction, making the interface more touch-friendly.
    - Adjusted the overall layout to center the feature buttons, aligning with the design goals for a central focus point.
    - Added labels with custom font sizes to accompany the icons, ensuring clarity of function for each feature represented by the buttons.
    - Refined the bottom bar to include back navigation and display critical information such as temperature and humidity.
    
    These changes aim to provide a more intuitive and user-friendly experience, with a visually appealing design that aligns with the latest UI trends. Further refinements and additional features will be incorporated in subsequent updates.

--- local-gui/main.py
+++ local-gui/main.py
@@ -1,141 +1,86 @@
-from kivy.app import App
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.image import Image
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.utils import get_color_from_hex
-from kivy.clock import Clock
-from kivy.properties import NumericProperty
+from kivy.core.window import Window
+from kivymd.app import MDApp
 
 Window.size = (800, 480)
 
 KV = '''
-#:import get_color_from_hex kivy.utils.get_color_from_hex
-
-<IconButton>:
-    font_size: '16dp'
-    size_hint: None, None
-    size: '150dp', '150dp'
-    # Placeholder for icon image
-    canvas.before:
-        Color:
-            rgba: get_color_from_hex('#ffffff')
-        Rectangle:
-            size: self.size
-            pos: self.pos
-            source: 'path/to/icon.png'
-
-<ClearButton@Button>:
-    background_normal: ''
-    background_color: (1, 1, 1, 0)  # RGBA: Fully transparent
-    color: get_color_from_hex('#FFFFFF')
-    size_hint: None, None
-    size: '120dp', '40dp'
-    font_size: '18dp'
-
-BoxLayout:
-    orientation: 'vertical'
-    padding: '20dp'
-    spacing: '20dp'
-    canvas.before:
-        Color:
-            rgba: get_color_from_hex('#000000')
-        Rectangle:
-            pos: self.pos
-            size: self.size
-
-    GridLayout:
-        cols: 4
-        size_hint_y: None
-        height: self.minimum_height
-        spacing: '20dp'
+Screen:
+    MDBoxLayout:
+        orientation: 'vertical'
+        spacing: dp(10)
         
-        IconButton:
-            text: 'Custom Profiles'
-            on_release: app.button_pressed('Custom Profiles')
-        IconButton:
-            text: 'Schedules'
-            on_release: app.button_pressed('Schedules')
-        IconButton:
-            text: 'Vacation'
-            on_release: app.button_pressed('Vacation')
-        IconButton:
-            text: 'Settings'
-            on_release: app.button_pressed('Settings')
-        IconButton:
-            text: 'Weather'
-            on_release: app.button_pressed('Weather')
-        IconButton:
-            text: 'Alerts and reminders'
-            on_release: app.button_pressed('Alerts and reminders')
-        IconButton:
-            text: 'System'
-            on_release: app.button_pressed('System')
-        IconButton:
-            text: 'Service'
-            on_release: app.button_pressed('Service')
+        Widget:
+            size_hint_y: None
+            height: self.parent.height * 0.2
 
-    BoxLayout:
-        size_hint_y: None
-        height: '1dp'
-        canvas.before:
-            Color:
-                rgba: 1, 1, 1, 1  # White color
-            Rectangle:
-                pos: self.pos
-                size: self.size
+        MDGridLayout:
+            cols: 4
+            size_hint: None, None
+            size: self.minimum_size
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+            spacing: dp(24)
+            padding: dp(24)
 
-    BoxLayout:
+            MDIconButton:
+                icon: 'thermostat'
+                user_font_size: '96sp' 
+                on_release: app.button_pressed('Comfort Profiles')
+            MDIconButton:
+                icon: 'calendar-today'
+                user_font_size: '96sp'
+                on_release: app.button_pressed('Schedules')
+            MDIconButton:
+                icon: 'beach'
+                user_font_size: '96sp'
+                on_release: app.button_pressed('Vacation')
+            MDIconButton:
+                icon: 'cogs'
+                user_font_size: '96sp'
+                on_release: app.button_pressed('Settings')
+            MDIconButton:
+                icon: 'weather-partly-cloudy'
+                user_font_size: '96sp'
+                on_release: app.button_pressed('Weather')
+            MDIconButton:
+                icon: 'bell-outline'
+                user_font_size: '96sp'
+                on_release: app.button_pressed('Alerts & Reminders')
+            MDIconButton:
+                icon: 'information-outline'
+                user_font_size: '96sp'
+                on_release: app.button_pressed('System')
+            MDIconButton:
+                icon: 'account-cog-outline'
+                user_font_size: '96sp'
+                on_release: app.button_pressed('Service')
+
+        # Bottom Spacer - reduced to take less space
+        Widget:
+            size_hint_y: None
+            height: self.parent.height * 0.2
+
+    MDBoxLayout:
         size_hint_y: None
         height: '60dp'
-        ClearButton:
-            text: 'Back'
+        padding: dp(8)
+        MDIconButton:
+            icon: 'arrow-left'
+            user_font_size: '48sp'
             on_release: app.footer_button_pressed('Back')
-        Widget:
-            size_hint_x: 1
-        ClearButton:
-            text: 'Home'
-            on_press: root.start_animation()
-            size: '140dp', '50dp'
-            font_size: '22dp'
-            on_release: app.go_to_home()
-
-        Widget:
-            size_hint_x: 1
-        ClearButton:
-            text: 'Done'
-            on_release: app.footer_button_pressed('Done')
+        MDLabel:
+            text: 'Temperature: 72°F | Humidity: 40%'
+            halign: 'center'
+            theme_text_color: 'Custom'
+            text_color: 1, 1, 1, 1
+            font_style: 'H5'
+            size_hint_x: None
+            width: self.texture_size[0] + dp(48)
 '''
 
-
-class MyAnimationApp(App):
-    index = NumericProperty(0) #keeping track of current frame
-    print(index)
-    max_index = 27 
-
+class SmartThermostatApp(MDApp):
     def build(self):
-        self.root = Builder.load_string(KV)
-        return self.root
-    
-    def animate(self, dt):
-        self.index += 1
-        if self.index > self.max_index:
-            self.index = 0 # reset 
-            print("im working!")
-        self.root.ids.gif.source = f'frame_{self.index}.png'
-
-    def start_anim(self):
-        Clock.schedule_interval(self.animate, 0.1)
-        print("Im working!")
-
-class IconButton(ButtonBehavior, Label):
-    pass
-
-class SmartThermostatApp(App):
-    def build(self):
+        self.theme_cls.theme_style = "Dark"  # will change to light if it looks better
         return Builder.load_string(KV)
 
     def button_pressed(self, button_text):
@@ -143,9 +88,6 @@ class SmartThermostatApp(App):
 
     def footer_button_pressed(self, button_text):
         print(f'{button_text} button pressed')
-
-    def go_to_home(self):
-        print('Home button pressed')
 
 if __name__ == '__main__':
     SmartThermostatApp().run()
