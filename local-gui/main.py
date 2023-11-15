5d351a0b1c98f1ca8939a1450178df287f7707fc
Emilio Laurenti <62228030+RedGaming4@users.noreply.github.com>
2023-11-15 13:12:05 -0500

    Notification bar has been added.
    Notification.

--- local-gui/main.py
+++ local-gui/main.py
@@ -4,9 +4,14 @@ from kivymd.app import MDApp
 from kivymd.uix.button import MDIconButton
 from kivymd.uix.label import MDLabel
 from kivy.properties import StringProperty
+from kivy.clock import Clock
+from kivy.animation import Animation
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.label import MDLabel
 
 Window.size = (800, 480)
 
+<<<<<<< Updated upstream
 KV = '''
 Screen:
     BoxLayout:
@@ -132,16 +137,27 @@ Screen:
 '''
 
 
+=======
+>>>>>>> Stashed changes
 class SmartThermostatApp(MDApp):
     recent_icon = StringProperty('history')
+    mj_icon = StringProperty('moon-full')
+    #notification_text = StringProperty('No New Notifications')
+    #def update_notification(self, new_message):
+        #self.notification_text = new_message
+
+    file_path = 'local-gui\main.kv'
+
     def build(self):
+        layout = BoxLayout(orientation='vertical')
+        notification_bar = NotificationBar()
+        layout.add_widget(notification_bar)
+        layout.add_widget(Builder.load_file(self.file_path))
+        self.theme_cls.theme_style_switch_animation = 0.8
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
         self.theme_cls.accent_palette = "LightBlue"
-        return Builder.load_string(KV)
-
-    def button_pressed(self, button_text):
-        print(f'{button_text} button pressed')
+        return Builder.load_file(self.file_path)
 
     def footer_button_pressed(self, button_text, next):
         print(f'{button_text} button pressed')
@@ -150,21 +166,52 @@ class SmartThermostatApp(MDApp):
         print(f'{button_text} button pressed')
         print(self.recent_icon)
         self.recent_icon = icon
+    
+    def on_start(self):
+        self.fps_monitor_start()
 
+    # These two functions handle the theme setting functionality and the "Sun" to "Moon" icon 
     def mj_button_pressed(self):
+        #checking if the theme is dark
         if self.theme_cls.theme_style == 'Dark':
             self.theme_cls.theme_style = 'Light'
-            print("Its light ")
+            self.mj_icon = 'weather-sunny'
+            self.root.clear_widgets()
+            self.root.add_widget(Builder.load_file(self.file_path))
         else:
             self.theme_cls.theme_style = 'Dark'
-            print("its dark")
-        self.root.clear_widgets()
-        self.root.add_widget(Builder.load_string(KV))
-        print(" UI has refreshed to apply new theme")
+            self.mj_icon = 'moon-full'
+            self.root.clear_widgets()
+            self.root.add_widget(Builder.load_file(self.file_path))
+            print(" UI has refreshed to apply new theme")
+        
+class NotificationBar(BoxLayout):
+    def __init__(self, **kwargs):
+        super(NotificationBar, self).__init__(**kwargs)
+        self.orientation = 'horizontal'
+        self.size_hint_y = None
+        self.height = 40  # Adjust as necessary
+        self.notification_label = MDLabel(size_hint_y=None, height=self.height,
+                                          halign="center", valign="middle")
+        self.add_widget(self.notification_label)
+        self.messages = ["Welcome", "AC Unit Running Smoothly", "Filter Needs Replacement"]
+        self.current_message = 0
+        self.animate_notification()
 
 
+    def animate_notification(self):
+        message = self.messages[self.current_message]
+        self.notification_label.text = message
+        self.current_message = (self.current_message + 1) % len(self.messages)
+        anim = Animation(opacity=0, duration=2) + Animation(opacity=1, duration=2)
+        anim.bind(on_complete=self.on_animation_complete)
+        anim.start(self.notification_label)
 
+    def on_animation_complete(self, *args):
+        self.animate_notification()
     
+    def update_notification(self, message):
+        self.notification_label.text = message
 
 if __name__ == '__main__':
     SmartThermostatApp().run()
