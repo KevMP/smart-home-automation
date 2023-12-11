34a1fcb0066e1f3a87e5eb635d7aec50c71e9e9d
workordeath <alfonso.oramas@icloud.com>
2023-12-11 15:34:52 -0500

    NEW: added two buttons to increase/decrease the temperature.

--- demo/simulation.py
+++ demo/simulation.py
@@ -1,12 +1,16 @@
 from model import Model
 import time
 import tkinter as tk
+from tkinter import font
 
 class Simulation:
     def __init__(self):
-        self.current_temperature = 0
+        self.lower_bound = 70 - 2
+        self.upper_bound = 71 + 2
+        self.current_temperature = self.lower_bound
         self.previous_temperature = self.current_temperature
-        self.preferred_temperatures = [70, 74]
+        self.preferred_temperatures = [70, 71]
+
     def getCurrentTemperature(self):
         return self.current_temperature
 
@@ -16,6 +20,8 @@ class Simulation:
             self.current_temperature += 1
         elif action == -1:
             self.current_temperature -= 1
+
+        self.current_temperature = max(self.lower_bound, min(self.current_temperature, self.upper_bound))
 
     def getDirection(self):
         return self.current_temperature - self.previous_temperature
@@ -37,14 +43,34 @@ class Simulation:
             else:
                 return -1
 
+    def increasePreferredTemperature(self):
+        self.preferred_temperatures = [temp + 1 for temp in self.preferred_temperatures]
+        self.lower_bound = self.preferred_temperatures[0] - 2
+        self.upper_bound = self.preferred_temperatures[1] + 2
+
+    def decreasePreferredTemperature(self):
+        self.preferred_temperatures = [temp - 1 for temp in self.preferred_temperatures]
+        self.lower_bound = self.preferred_temperatures[0] - 2
+        self.upper_bound = self.preferred_temperatures[1] + 2
+
+
 class App:
     def __init__(self, master):
         self.master = master
         self.master.title("Temperature Monitor")
         self.master.geometry("500x500")
 
-        self.temperature_label = tk.Label(master, text="Current Temperature: 0")
-        self.temperature_label.pack()
+        # Set a larger font for the temperature label
+        self.temperature_font = font.Font(family="Helvetica", size=20)
+
+        self.temperature_label = tk.Label(master, text="Current Temperature: 0", font=self.temperature_font)
+        self.temperature_label.grid(row=0, column=0, columnspan=2, pady=10)
+
+        self.increase_button = tk.Button(master, text="Increase Temperature", command=self.increase_temperature)
+        self.increase_button.grid(row=1, column=0, padx=10, pady=10)
+
+        self.decrease_button = tk.Button(master, text="Decrease Temperature", command=self.decrease_temperature)
+        self.decrease_button.grid(row=1, column=1, padx=10, pady=10)
 
         self.ai = Model()
         self.environment = Simulation()
@@ -62,11 +88,19 @@ class App:
         self.ai.reward(self.environment.isAiGettingCloserToTarget())
         self.master.after(int(self.CYCLE_SPEED * 1000), self.run_simulation)
 
+    def increase_temperature(self):
+        self.environment.increasePreferredTemperature()
+
+    def decrease_temperature(self):
+        self.environment.decreasePreferredTemperature()
+
+
 def main():
     root = tk.Tk()
     app = App(root)
     root.after(int(app.CYCLE_SPEED * 1000), app.run_simulation)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
