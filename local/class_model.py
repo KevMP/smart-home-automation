6a7dda2f98329454c1adfc5f7f96b98af666df6d
workordeath <alfonso.oramas@icloud.com>
2023-12-17 12:57:26 -0500

    FORMAT: renamed layer.

--- local/class_model.py
+++ local/class_model.py
@@ -1,3 +1,3 @@
 class Model:
-    def __init__(self, input_layer: int):
-        self.action_matrix = [0] * input_layer
+    def __init__(self, output_layer: int):
+        self.action_matrix = [0] * output_layer
