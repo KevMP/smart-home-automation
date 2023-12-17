0a688b3fdaf7f2b7928fc4d38cca22ab2e3a5c31
workordeath <alfonso.oramas@icloud.com>
2023-12-17 14:29:06 -0500

    WIP: pseudo functions added.

--- local/ai_airconditioner_reward_system.py
+++ local/ai_airconditioner_reward_system.py
@@ -4,3 +4,12 @@ class AirconditionerReward(Reward):
     def __init__(self, model):
         super().__init__()
         self.model = model
+        self.real_temperature = 0.0
+    
+    def getTemperatureFromDatabase():
+        pass
+    def getHumidityFromDatabase():
+        pass
+
+    def calculateRealTemperature():
+        pass
