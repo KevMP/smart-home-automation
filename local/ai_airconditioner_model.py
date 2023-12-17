163ace3b667824dc9e031a0dbc5f0d1e625db253
workordeath <alfonso.oramas@icloud.com>
2023-12-17 16:21:50 -0500

    FORMAT: changed variables to snake case.

--- local/ai_airconditioner_model.py
+++ local/ai_airconditioner_model.py
@@ -3,15 +3,15 @@ from class_model import *
 class AirconditionerModel(Model):
     def __init__(self):
         super().__init__()
-        self.airconditionerModel = Model()
-        self.airconditionerModel.addDecision("raise")
-        self.airconditionerModel.addDecision("lower")
-        self.airconditionerModel.addDecision("do_nothing")
+        self.airconditioner_model = Model()
+        self.airconditioner_model.addDecision("raise")
+        self.airconditioner_model.addDecision("lower")
+        self.airconditioner_model.addDecision("do_nothing")
 
-        self.averageTemperature = 0.0
-        self.averageHumidity = 0.0
-        self.feelsLikeTemperature = 0.0
-        self.currentProfile = ''
+        self.average_temperature = 0.0
+        self.average_humidity = 0.0
+        self.feels_like_temperature = 0.0
+        self.current_profile = ''
     
     def getAverageTemperature(self):
         if not self.database_connection:
@@ -27,7 +27,7 @@ class AirconditionerModel(Model):
 
             result = self.cursor.execute(query).fetchone()
             if result:
-                self.averageTemperature = result[1]
+                self.average_temperature = result[1]
 
         except sql.Error as e:
             print(f"Error fetching data: {e}")
@@ -47,7 +47,7 @@ class AirconditionerModel(Model):
 
             result = self.cursor.execute(query).fetchone()
             if result:
-                self.averageHumidity = result[1]
+                self.average_humidity = result[1]
 
         except sql.Error as e:
             print(f"Error fetching data: {e}")
@@ -61,12 +61,12 @@ class AirconditionerModel(Model):
         self.coefficients = [-42.379, 2.04901523, 10.14333127, -0.22475541, -6.83783e-3,
             -5.481717e-2, 1.22874e-3, 8.5282e-4, -1.99e-6]
         
-        self.relative_humidity = self.averageHumidity / 100.0
+        self.relative_humidity = self.average_humidity / 100.0
 
-        self.feelsLikeTemperature = self.coefficients[0] + self.coefficients[1] * self.averageTemperature + self.coefficients[2] * self.relative_humidity + \
-                    self.coefficients[3] * self.averageTemperature  * self.relative_humidity + self.coefficients[4] * self.averageTemperature **2 + \
-                    self.coefficients[5] * self.relative_humidity**2 + self.coefficients[6] * self.averageTemperature **2 * self.relative_humidity + \
-                    self.coefficients[7] * self.averageTemperature  * self.relative_humidity**2 + self.coefficients[8] * self.averageTemperature **2 * self.relative_humidity**2
+        self.feels_like_temperature = self.coefficients[0] + self.coefficients[1] * self.average_temperature + self.coefficients[2] * self.relative_humidity + \
+                    self.coefficients[3] * self.average_temperature  * self.relative_humidity + self.coefficients[4] * self.average_temperature **2 + \
+                    self.coefficients[5] * self.relative_humidity**2 + self.coefficients[6] * self.average_temperature **2 * self.relative_humidity + \
+                    self.coefficients[7] * self.average_temperature  * self.relative_humidity**2 + self.coefficients[8] * self.average_temperature **2 * self.relative_humidity**2
 
     def getProfile(self):
         pass
@@ -77,7 +77,7 @@ model = AirconditionerModel()
 model.getAverageTemperature()
 model.getAverageHumidity()
 model.closeConnection()
-print(model.averageTemperature)
-print(model.averageHumidity)
+print(model.average_temperature)
+print(model.average_humidity)
 model.calculateFeelsLikeTemperature()
-print(model.feelsLikeTemperature)
+print(model.feels_like_temperature)
