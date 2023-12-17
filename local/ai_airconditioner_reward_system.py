1d37a651dfef5c9598c1d4d65f03c0938d509273
workordeath <alfonso.oramas@icloud.com>
2023-12-17 13:43:13 -0500

    NEW: function to reward or punish the model based on its current and previous reward values.

--- local/ai_airconditioner_reward_system.py
+++ local/ai_airconditioner_reward_system.py
@@ -1,5 +1,5 @@
-from ai_airconditioner_model import establish_temperature_model
+import ai_airconditioner_model as ac_model
 
-model = establish_temperature_model()
+model = ac_model.establish_temperature_model()
 print(model.action_matrix)
 print(model.decision_tree)
--- local/class_reward_system.py
+++ local/class_reward_system.py
@@ -4,4 +4,8 @@ class Reward:
         self.previous_reward = 0
         self.model = model
 
-    
+    def rewardOrPunishModel(self):
+        if self.current_reward > self.previous_reward:
+            return True
+        else:
+            return False
