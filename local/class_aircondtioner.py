6777ec27c03c2b9490f0f9969dc046b00122daa0
JoseRDGZ <josecrodriguez2004@gmail.com>
2023-12-17 20:32:37 -0500

    Update class_aircondtioner.py WIP

--- local/class_aircondtioner.py
+++ local/class_aircondtioner.py
@@ -4,26 +4,26 @@ from datetime import datetime
 class Airconditioner(Database):
     def __init__(self):
         super().__init__()
-        self.ai_command = self.get_command_from_ai()  # Fetch and store the AI command
+        self.command_to_ac = self.get_command_from_ai()  # Fetch and store the AI command
 
     def get_command_from_ai(self):
-        select_query = "SELECT command FROM ai_command_table LIMIT 1;"
+        select_query = "SELECT command_to_AC FROM Temperature_Mode ORDER BY timestamp DESC LIMIT 1;"
         self.cursor.execute(select_query)
         result = self.cursor.fetchone()
         if result:
             return result[0]
         else:
-            print("No AI command found.")
+            print("No recent command found.")
             return None
 
     def write_command_to_ac(self):
-        if self.ai_command:
+        if self.command_to_ac:
             current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            insert_query = "INSERT INTO air_conditioner (timestamp, command) VALUES (?, ?);"
-            self.cursor.execute(insert_query, (current_timestamp, self.ai_command))
+            insert_query = "INSERT INTO Temperature_Mode (timestamp, command_to_AC) VALUES (?, ?);"
+            self.cursor.execute(insert_query, (current_timestamp, self.command_to_ac))
             self.database_connection.commit()
         else:
-            print("No AI command to write to the AC table.")
+            print("No command to write to the Temperature_Mode table.")
 
     def close_connection(self):
         self.closeConnection()
@@ -31,10 +31,10 @@ class Airconditioner(Database):
 def test():
     print("CREATING OBJECT")
     ac = Airconditioner()
-    print("GETTING FURTHER COMMANDS")
-    ac.get_command_from_ai()
     print("WRITING TO DATABASE")
     ac.write_command_to_ac()
+    print("CLOSING CONNECTION")
+    ac.close_connection()
 
 if __name__ == "__main__":
     test()
