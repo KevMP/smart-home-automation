c9e754bd8c5cc380a4f8181ecd7717c8dd67edb4
JoseRDQZ <101509445+JoseRDQZ@users.noreply.github.com>
2023-12-17 19:31:23 -0500

    Update class_aircondtioner.py WIP

--- local/class_aircondtioner.py
+++ local/class_aircondtioner.py
@@ -1,21 +1,35 @@
 from database_root import *
-"""
-Get ai command from the table in the database.
-keep it in the init variable.
-
-Functions that this class needs to do.
-
-* Get the command from the ai table in the database.db file.
-* Save the command from the ai into a variable inside the class.
-* Write the command to the air conditioner class table.
-"""
+from datetime import datetime
 
 class Airconditioner(Database):
     def __init__(self, name: str):
         super().__init__()
+        self.name = name
+        self.ai_command = self.get_command_from_ai()
+
+    def get_command_from_ai(self):
+        # Fetch the AI command from the ai_command_table using the air conditioner's name
+        # The exact table name and column name should match your database schema
+        select_query = "SELECT command FROM ai_command_table WHERE ac_name = ?;"
+        self.cursor.execute(select_query, (self.name,))
+        result = self.cursor.fetchone()
+        if result:
+            return result[0]
+        else:
+            print(f"No AI command found for the air conditioner named {self.name}.")
+            return None
+
+    def write_command_to_ac(self):
+        # Insert the AI command into the air_conditioner table with the current timestamp
+        if self.ai_command:
+            # Get the current date and time in the format for a SQL timestamp
+            current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            insert_query = "INSERT INTO air_conditioner (timestamp, command) VALUES (?, ?);"
+            self.cursor.execute(insert_query, (current_timestamp, self.ai_command))
+            self.database_connection.commit()
+        else:
+            print("No AI command to write to the AC table.")
     
-    """
-    to close the data base connection,
-    call this function:
-    self.closeConnection()
-    """
+    def __del__(self):
+        # Ensure the database connection is closed when the object is deleted
+        self.closeConnection()
