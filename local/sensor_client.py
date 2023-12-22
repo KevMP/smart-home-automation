469ef8fdde01214a5d3c5fa3bee52003669a29bb
workordeath <alfonso.oramas@icloud.com>
2023-12-22 13:25:06 -0500

    NEW: documented the server/client communications.

--- local/sensor_client.py
+++ local/sensor_client.py
@@ -21,7 +21,17 @@ def main():
     sensor1 = sensorConstructor(1)
     sensor2 = sensorConstructor(2)
     list_of_sensors = [sensor0, sensor1, sensor2]
+    """
+    **********************************************************************************
+    The following is going to be sending data from multiple
+    sensors, the list above is so that we can expand how many
+    sensors we are sending to the database.
 
+    However sensor specific data still needs to be built, currently
+    it is only capturing sensor data from one sensor, or however
+    the getTemperature and getHumidity functions are made.
+    **********************************************************************************
+    """
     while True:
         for sensor in list_of_sensors:
             while server_data != "CONTINUE":
--- local/sensor_server.py
+++ local/sensor_server.py
@@ -11,15 +11,23 @@ def main():
     client, client_address = server.acceptClient()
 
     sensor_object = Sensor(None)
+    """
+    **********************************************************************************
+    The following server is going to send the client a message that allows the client
+    to continue or not, after the client receives a list of data it will then move on
+    and write that data.
+
+    This is to prevent a data overflow, where one socket gets much more data leading
+    to a "corruption" of sorts on the receiving data.
+    **********************************************************************************
+    """
     while True:
         server.sendData(client, "CONTINUE")
 
         client_data = server.getDataAsList(client)
         while client_data == '':
             client_data = server.getDataAsList(client)
-        print("CLIENT DATA CAPTURED:")
-        print(f"{client_data}, {client_data[0]}, {client_data[1]}, {client_data[2]}")
-        
+
         sensor_object.setSensorId(client_data[0])
         sensor_object.setTemperature(client_data[1])
         sensor_object.setHumidity(client_data[2])
