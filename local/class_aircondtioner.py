8e809c9b40712c3cbae43a2db06f8969fe0cf7d0
JoseRDQZ <101509445+JoseRDQZ@users.noreply.github.com>
2023-12-21 20:53:46 -0500

    Updated class_aircondtioner.py

--- local/class_aircondtioner.py
+++ local/class_aircondtioner.py
@@ -1,22 +1,23 @@
 from database_root import *
 
 class Airconditioner(Database):
-    def __init__(self, command_to_ac=None):
+    def __init__(self):
         super().__init__()
-        self.command_to_ac = command_to_ac  # Initialize the command_to_ac attribute
+        self.command_to_ac = ""  # Initialize command_to_ac to an empty string
 
     def get_command_from_ai(self):
         select_query = "SELECT command FROM TemperatureModel ORDER BY timestamp DESC LIMIT 1;"
         self.cursor.execute(select_query)
         result = self.cursor.fetchone()
         if result:
-            return result[0]
+            self.command_to_ac = result[0]  # Set command_to_ac with the command from AI
+            return self.command_to_ac
         else:
             print("No recent command found.")
             return None
 
     def write_command_to_ac(self):
-        if self.command_to_ac is not None:  # Check if command_to_ac is not None instead of if it exists
+        if self.command_to_ac:  # Check if command_to_ac has been set by get_command_from_ai
             insert_query = "INSERT INTO Airconditioner (command) VALUES (?);"
             self.cursor.execute(insert_query, (self.command_to_ac,))
             self.database_connection.commit()
@@ -28,10 +29,11 @@ class Airconditioner(Database):
 
 def test():
     print("CREATING OBJECT")
-    command = 'your_command_here'  # Replace with the actual command
-    ac = Airconditioner(command)
+    ac = Airconditioner()
+    print("GETTING COMMAND FROM AI")
+    ac.get_command_from_ai()  # This will attempt to get the command and set it
     print("WRITING TO DATABASE")
-    ac.write_command_to_ac()
+    ac.write_command_to_ac()  # This will write the command if it was successfully retrieved
     print("CLOSING CONNECTION")
     ac.close_connection()
 
