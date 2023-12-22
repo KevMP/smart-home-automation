34215209d1c16202d78c09427102f450df312665
JoseRDQZ <101509445+JoseRDQZ@users.noreply.github.com>
2023-12-21 20:36:50 -0500

    Update class_aircondtioner.py Finished

--- local/class_aircondtioner.py
+++ local/class_aircondtioner.py
@@ -1,13 +1,12 @@
 from database_root import *
-from datetime import datetime
 
 class Airconditioner(Database):
-    def __init__(self):
+    def __init__(self, command_to_ac=None):
         super().__init__()
-        self.command_to_ac = self.get_command_from_ai()  # Fetch and store the AI command
+        self.command_to_ac = command_to_ac  # Initialize the command_to_ac attribute
 
     def get_command_from_ai(self):
-        select_query = "SELECT command_to_AC FROM Temperature_Mode ORDER BY timestamp DESC LIMIT 1;"
+        select_query = "SELECT command FROM TemperatureModel ORDER BY timestamp DESC LIMIT 1;"
         self.cursor.execute(select_query)
         result = self.cursor.fetchone()
         if result:
@@ -17,20 +16,20 @@ class Airconditioner(Database):
             return None
 
     def write_command_to_ac(self):
-        if self.command_to_ac:
-            current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            insert_query = "INSERT INTO Temperature_Mode (timestamp, command_to_AC) VALUES (?, ?);"
-            self.cursor.execute(insert_query, (current_timestamp, self.command_to_ac))
+        if self.command_to_ac is not None:  # Check if command_to_ac is not None instead of if it exists
+            insert_query = "INSERT INTO Airconditioner (command) VALUES (?);"
+            self.cursor.execute(insert_query, (self.command_to_ac,))
             self.database_connection.commit()
         else:
-            print("No command to write to the Temperature_Mode table.")
+            print("No command to write to the Airconditioner table.")
 
     def close_connection(self):
         self.closeConnection()
 
 def test():
     print("CREATING OBJECT")
-    ac = Airconditioner()
+    command = 'your_command_here'  # Replace with the actual command
+    ac = Airconditioner(command)
     print("WRITING TO DATABASE")
     ac.write_command_to_ac()
     print("CLOSING CONNECTION")
