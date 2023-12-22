9d0caa964f7376f8cb04c4753e47c747839b1638
JoseRDQZ <101509445+JoseRDQZ@users.noreply.github.com>
2023-12-21 21:34:26 -0500

    Update database_aircondtioner_scanner.py
    Remove Logging

--- local/database_aircondtioner_scanner.py
+++ local/database_aircondtioner_scanner.py
@@ -1,10 +1,5 @@
 from database_root import *
 import time
-import logging
-
-# Configure logging
-logging.basicConfig(filename='airconditioner_commands.log', level=logging.INFO, 
-                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 class Airconditioner(Database):
     def __init__(self):
@@ -12,15 +7,14 @@ class Airconditioner(Database):
         self.command_to_ac = ""  # Initialize command_to_ac to an empty string
 
     def get_command_from_ai(self):
-        select_query = "SELECT airconditioner_command FROM TemperatureModel ORDER BY timestamp DESC LIMIT 1;"
+        select_query = "SELECT command FROM TemperatureModel ORDER BY timestamp DESC LIMIT 1;"
         self.cursor.execute(select_query)
         result = self.cursor.fetchone()
         if result:
             self.command_to_ac = result[0]  # Set command_to_ac with the command from AI
-            logging.info(f"Command retrieved from AI: {self.command_to_ac}")
             return self.command_to_ac
         else:
-            logging.info("No recent command found.")
+            # If no command found, you might want to handle this case.
             return None
 
     def write_command_to_ac(self):
@@ -28,33 +22,28 @@ class Airconditioner(Database):
             insert_query = "INSERT INTO Airconditioner (command) VALUES (?);"
             self.cursor.execute(insert_query, (self.command_to_ac,))
             self.database_connection.commit()
-            logging.info(f"Command written to the database: {self.command_to_ac}")
+            # Command has been written to the database. If you need to do something here, add your code.
         else:
-            logging.info("No command to write to the Airconditioner table.")
+            # If no command to write, handle this case if necessary.
+            pass
 
     def close_connection(self):
         self.closeConnection()
 
 def database_airconditioner_scanner(scan_interval=0.5):  # Scan every half second
-    """
-    Function to continuously scan for commands from the AI and log them.
-    
-    :param scan_interval: Time in seconds between each scan
-    """
     ac = Airconditioner()
     try:
         while True:
             command = ac.get_command_from_ai()
-            print(f"COMMAND FROM AI {command}")
             if command:
                 ac.write_command_to_ac()
-                print(f"{command} WRITTEN TO AIR CONDITIONER DATABASE")
             time.sleep(scan_interval)  # Wait for half a second before the next scan
     except KeyboardInterrupt:
-        logging.info("Scanner stopped manually.")
+        # Here you can handle the manual stop if needed.
+        pass
     finally:
         ac.close_connection()
-        logging.info("Database connection closed.")
+        # Here you can handle the closure of database connection if needed.
 
 if __name__ == "__main__":
     database_airconditioner_scanner()
