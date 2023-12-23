dfe733d9af2258e6d4b932265c481a7f50aad9f7
workordeath <alfonso.oramas@icloud.com>
2023-12-23 10:59:55 -0500

    NEW: added functionality for the client class to send requests to the server and await a continue flag from the server.

--- local/class_network_client.py
+++ local/class_network_client.py
@@ -1,5 +1,4 @@
 import socket
-import time
 
 class Client:
     def __init__(self, ip: str="127.0.0.1", port: int=5000):
@@ -9,8 +8,27 @@ class Client:
 
     def connectToServer(self):
         self.client.connect((self.ip, self.port))
-    
+    """
+    The following function is meant to serve as a
+    buffer for our client/server communication,
+    this is to prevent the client from bombarding
+    the server with data.
+
+    it will await a continue flag from our server,
+    and until that happens, our client won't be sending
+    any data.
+    """
+    def waitForServerContinueFlag(client):
+        client_data = client.getData()
+        while client_data != "CONTINUE":
+            client_data = client.getData()
+        print("CONTINUE CAPTURED")
+
     def getData(self):
         return self.client.recv(1024).decode('utf-8')
     def sendData(self, data):
         self.client.sendall(data.encode('utf-8'))
+    def sendWriteFlag(self):
+        self.sendData("WRITE")
+    def sendReadflag(self):
+        self.sendData("READ")
