c77caef591d120e3c742ae373808c1210f65d487
workordeath <alfonso.oramas@icloud.com>
2023-12-23 10:45:11 -0500

    NEW: documentation for the database_server.

--- local/database_server.py
+++ local/database_server.py
@@ -9,11 +9,20 @@ def main():
     server = Server()
     server.bindServer()
     server.listenForConnection(AMOUNT_OF_CLIENTS)
+    """
+    Our server waits for every available client to connect,
+    to add more clients, simply change the constant value of
+    the AMOUNT_OF_CLIENTS.
 
+    This is so that we can use a round-robin algorithm to
+    allow for each client to send/receive data in a sequential
+    format.
+    """
     connected_clients = []
     for connected_client in range(AMOUNT_OF_CLIENTS):
         client, client_address = server.acceptClient()
         connected_clients.append(client)
+    
     while True:
         for connected_client in connected_clients:
             server.sendData(connected_client, "CONTINUE")
