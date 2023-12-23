04fd330d34e96a75b7818117beba4dd41fe3907d
workordeath <alfonso.oramas@icloud.com>
2023-12-23 14:29:39 -0500

    DEL: removed time intervals since our scheduler is going to be handling that indirectly.

--- local/database_aircondtioner_scanner.py
+++ local/database_aircondtioner_scanner.py
@@ -1,21 +1,13 @@
 from class_aircondtioner import *
 import time
 
-def database_airconditioner_scanner(scan_interval=0.5):  # Scan every half second
+def database_airconditioner_scanner():
     ac = Airconditioner()
-    try:
-        while True:
-            command = ac.get_command_from_ai()
-            if command:
-                ac.write_command_to_ac()
-                print(f"COMMAND: {command} WRITTEN TO THE AIRCONDITIONER TABLE")
-            time.sleep(scan_interval)  # Wait for half a second before the next scan
-    except KeyboardInterrupt:
-        # Here you can handle the manual stop if needed.
-        pass
-    finally:
-        ac.close_connection()
-        # Here you can handle the closure of database connection if needed.
+    while True:
+        command = ac.get_command_from_ai()
+        if command:
+            ac.write_command_to_ac()
+            print(f"COMMAND: {command} WRITTEN TO THE AIRCONDITIONER TABLE")
 
 if __name__ == "__main__":
     database_airconditioner_scanner()
