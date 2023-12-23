c49478260be3524e0e6f6fabbd66ed695fe9bc99
workordeath <alfonso.oramas@icloud.com>
2023-12-23 15:59:07 -0500

    NEW: imported client class into local-gui, established a gui_client file as well.

---
+++ local-gui/class_network_client.py
@@ -0,0 +1,39 @@
+import socket
+"""
+Since this Gui is in its own folder, I just copy/pasted
+the entire class here from the local folder.
+"""
+class Client:
+    def __init__(self, ip: str="127.0.0.1", port: int=5000):
+        self.ip = ip
+        self.port = port
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connectToServer(self):
+        self.client.connect((self.ip, self.port))
+    """
+    The following function is meant to serve as a
+    buffer for our client/server communication,
+    this is to prevent the client from bombarding
+    the server with data.
+
+    it will await a continue flag from our server,
+    and until that happens, our client won't be sending
+    any data.
+    """
+    def waitForServerContinueFlag(self, client):
+        client_data = self.getData()
+        while client_data != "CONTINUE":
+            client_data = self.client.getData()
+        print("CONTINUE CAPTURED")
+
+    def getData(self):
+        return self.client.recv(1024).decode('utf-8')
+    def sendData(self, data):
+        self.client.sendall(data.encode('utf-8'))
+    def sendWriteFlag(self, client):
+        self.sendData("WRITE")
+        self.waitForServerContinueFlag(client)
+    def sendReadFlag(self, client):
+        self.sendData("READ")
+        self.waitForServerContinueFlag(client)
