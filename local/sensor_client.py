b998eef493757aff76cd3a216c31c824d89cc28f
workordeath <alfonso.oramas@icloud.com>
2024-01-24 11:48:36 -0500

    NEW: DHT11 sensor functionality for the raspberry pi, note that now this sensor file can not be used as faux data, for it to work it has to be configured and wired to the sensor itself.

--- local/sensor_client.py
+++ local/sensor_client.py
@@ -1,10 +1,16 @@
 from class_network_client import *
 from class_sensor import *
+import Adafruit_DHT
 
-def getSensorData():
-    return 10, 12
+
+def getSensorData(DHT_SENSOR, DHT_PIN: int):
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        return humidity, temperature
 
 def main():
+    DHT_SENSOR = Adafruit_DHT.DHT11
+    DHT_PIN = 4 ## The GPIO pin
     sensor = Sensor(0)
 
     client = Client() ## Configure the IPv4 address and PORT number to the Client Handler.
@@ -12,7 +18,7 @@ def main():
     while True:
         client.sendWriteFlag(client)
         
-        humidity, temperature = getSensorData()
+        humidity, temperature = getSensorData(DHT_SENSOR, DHT_PIN)
         sensor.setHumidity(humidity)
         sensor.setTemperature(temperature)
         sensor_query = sensor.createSensorQuery()
