c7c7c05cbb103d8f952a7e4cf454fda0a39b943d
workordeath <alfonso.oramas@icloud.com>
2024-03-09 11:06:10 -0500

    NEW: Established a button client responsible for getting the commands.

---
+++ local-gui/gui_client_buttons.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+from gpiozero import Button
+from class_network_client import *
+
+class SmartThermostatApp(tk.Tk):
+    def __init__(self, client):
+        super().__init__()
+
+        self.title("Smart AI Thermostat")
+        self.geometry("300x100")
+
+        self.button_action = None
+
+        self.temperature_label = ttk.Label(self, text=f"AGENT_COMMAND: {self.button_action}", font=("Arial", 14))
+        self.temperature_label.pack(pady=10)
+
+        self.client = client
+
+    def getButtonAction(self):
+        pass
+
+if __name__ == '__main__':
+    client = Client()
+    client.connectToServer()
+
+    app = SmartThermostatApp(client)
+    app.mainloop()
