8fe0764172a15b39cfdacd5e3c2f4d1e4b389e2e
workordeath <alfonso.oramas@icloud.com>
2024-03-26 20:32:01 -0400

    NEW: Established some example functions of how the embedded communication will look like on the

---
+++ local/airconditioner_controller.py
@@ -0,0 +1,32 @@
+from class_network_client import *
+
+def writeCommandToAirconditioner(command):
+    ## This function writes a command to the air conditioner.
+    relay_address = 0xF1 ## change to whatever the relay Address is set to.
+    if command == "increase":
+        on  = 10000000 ## Binary value of 1 example.
+        write_data_byte_to_relay(on, relay_address)
+    elif command == "decrease":
+        off = 00000000 ## Binary value of 0 example.
+        write_data_byte_to_relay(off, relay_address)
+
+def isAirconditionerOn():
+    ## This function should return a true/false based
+    ## on if the airconditioner is activated or not.
+    airconditioner_status = False
+    if airconditioner_status == True:
+        return True
+    else:
+        return False
+
+def getCommandFromDatabase():
+    query = """
+            SELECT command 
+            FROM Airconditioner 
+            ORDER BY timestamp DESC 
+            LIMIT 1;
+            """
+    return query
+
+def main():
+    client = Client("127.0.0.1", 5000) ## Configure to correct addresses.
